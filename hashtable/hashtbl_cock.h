4dc9a22468f5f72c570e5d02a20233ca93ee54e7
Hoang-Vu Dang <dang.hvu@gmail.com>
2016-10-31 14:23:10 -0500

    Missing files

---
+++ hashtable/hashtbl_cock.h
@@ -0,0 +1,24 @@
+#ifndef COCK_HASHTBL_H_
+#define COCK_HASHTBL_H_
+
+#include "hashtbl.h"
+#include <libcuckoo/cuckoohash_map.hh>
+
+class cock_hashtbl : base_hashtbl {
+ public:
+  void init() override {}
+
+  bool insert(const key_type& key, value_type& value) override {
+    bool ret = true;
+    tbl_.upsert(key, [&ret, &value](mpiv_value& v) { value.v = v.v; ret = false; }, value);
+    if (!ret) {
+      tbl_.erase(key);
+    }
+    return ret;
+  }
+
+ private:
+  cuckoohash_map<key_type, value_type> tbl_;
+};
+
+#endif
---
+++ hashtable/hashtbl_lf.h
@@ -0,0 +1,29 @@
+#ifndef LF_HASHTBL_H_
+#define LF_HASHTBL_H_
+
+#include "hashtbl.h"
+
+extern "C" {
+#include "lf_hash.h"
+}
+
+class lf_hashtbl : base_hashtbl {
+ public:
+  void init() override { tbl_ = qt_hash_create(0); }
+
+  bool insert(const key_type& key, value_type& value) override {
+    value_type ret;
+    ret.v = qt_hash_put(tbl_, (const void*)key, value.v);
+    if (ret.v == value.v) return true;
+    else {
+      value = ret;
+      qt_hash_remove(tbl_, (const void*)key);
+      return false;
+    }
+  }
+
+ private:
+  qt_hash tbl_;
+};
+
+#endif
