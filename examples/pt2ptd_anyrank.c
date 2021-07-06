9759578ea977caeb2f07ae336e165c9735ea4862
kodingkoning <koningelizabeth@gmail.com>
2021-07-05 21:27:51 -0500

    example for any rank direct msg

---
+++ examples/pt2ptd_anyrank.c
@@ -0,0 +1,85 @@
+#include "lci.h"
+#include <stdio.h>
+#include <assert.h>
+#include <string.h>
+#include <unistd.h>
+
+#include "comm_exp.h"
+
+#undef MAX_MSG
+#define MAX_MSG (4 * 1024 * 1024)
+
+int total = TOTAL;
+int skip = SKIP;
+
+int main(int argc, char** args) {
+  LCI_initialize(&argc, &args);
+  LCI_endpoint_t ep;
+  LCI_PL_t prop;
+  LCI_PL_create(&prop);
+  LCI_MT_t mt;
+  LCI_MT_create(0, &mt);
+  LCI_PL_set_match_type(LCI_MATCH_TAG, &prop);
+  LCI_PL_set_mt(&mt, &prop);
+  LCI_endpoint_create(0, prop, &ep);
+
+  int rank = LCI_RANK;
+  int tag = 99;
+
+  LCI_syncl_t sync;
+
+  double t1 = 0;
+  size_t alignment = sysconf(_SC_PAGESIZE);
+  void* src_buf = 0;
+  void* dst_buf = 0;
+  posix_memalign(&src_buf, alignment, MAX_MSG);
+  posix_memalign(&dst_buf, alignment, MAX_MSG);
+
+  if (rank == 0) {
+    for (int size = MIN_MSG; size <= MAX_MSG; size <<= 1) {
+      memset(src_buf, 'a', size);
+      memset(dst_buf, 'b', size);
+
+      if (size > LARGE) { total = TOTAL_LARGE; skip = SKIP_LARGE; }
+
+      for (int i = 0; i < total + skip; i++) {
+        if (i == skip) t1 = wtime();
+        LCI_one2one_set_empty(&sync);
+        while (LCI_sendd(src_buf, size, -1, tag, ep, &sync) != LCI_OK)
+          LCI_progress(0, 1);
+        while (LCI_one2one_test_empty(&sync))
+          LCI_progress(0, 1);
+        LCI_one2one_set_empty(&sync);
+        LCI_recvd(dst_buf, size, -1, tag, ep, &sync);
+        while (LCI_one2one_test_empty(&sync))
+          LCI_progress(0, 1);
+        if (i == 0) {
+          for (int j = 0; j < size; j++)
+            assert(((char*) src_buf)[j] == 'a' && ((char*)dst_buf)[j] == 'a');
+        }
+      }
+
+      t1 = 1e6 * (wtime() - t1) / total / 2;
+      printf("%10.d %10.3f\n", size, t1);
+    }
+  } else {
+    for (int size = MIN_MSG; size <= MAX_MSG; size <<= 1) {
+      memset(src_buf, 'a', size);
+      memset(dst_buf, 'b', size);
+      if (size > LARGE) { total = TOTAL_LARGE; skip = SKIP_LARGE; }
+
+      for (int i = 0; i < total + skip; i++) {
+        LCI_one2one_set_empty(&sync);
+        LCI_recvd(dst_buf, size, -1, tag, ep, &sync);
+        while (LCI_one2one_test_empty(&sync))
+          LCI_progress(0, 1);
+        LCI_one2one_set_empty(&sync);
+        while (LCI_sendd(src_buf, size, -1, tag, ep, &sync) != LCI_OK)
+          LCI_progress(0, 1);
+        while (LCI_one2one_test_empty(&sync))
+          LCI_progress(0, 1);
+      }
+    }
+  }
+  LCI_finalize();
+}
