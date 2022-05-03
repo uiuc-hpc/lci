4e5e6e6a6bd79ee670ed51327217db9b96aa0aa4
Jiakun Yan <jiakuny3@illinois.com>
2022-05-03 15:57:22 -0500

    fix LCI_USE_DREG

---
+++ src/misc.c
@@ -0,0 +1,16 @@
+#include "lcii_config.h"
+#include "lci.h"
+#include "lcii.h"
+
+#ifdef LCI_USE_DREG
+uintptr_t get_dma_mem(void* server, void* buf, size_t s)
+{
+  return _real_server_reg(server, buf, s);
+}
+
+int free_dma_mem(uintptr_t mem)
+{
+  _real_server_dereg(mem);
+  return 1;
+}
+#endif
