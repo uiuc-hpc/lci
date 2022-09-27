9a0691ebf3e734374b12e2921e74c1c5b43fefcd
Jiakun Yan <jiakunyan1998@gmail.com>
2022-09-27 14:34:20 -0500

    slightly improve profile_wrapper

--- experiments/expanse/profile/profile_wrapper.sh
+++ experiments/expanse/profile/profile_wrapper.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-perf record --freq=997 --call-graph dwarf -q -o perf.data.$SLURM_PROCID ${PATH_TO_EXE}/lcitb_pt2pt --op 2m --nthreads 16 --thread-pin 1
+echo "perf record --freq=997 --call-graph dwarf -q -o perf.data.$SLURM_JOB_ID ${PATH_TO_EXE}/lcitb_pt2pt --op 2l --send-comp-type=sync --recv-comp-type=sync --nthreads 16 --thread-pin 1 --nsteps=100000 --max-msg-size=8"
+perf record --freq=997 --call-graph dwarf -q -o perf.data.$SLURM_JOB_ID ${PATH_TO_EXE}/lcitb_pt2pt --op 2l --send-comp-type=sync --recv-comp-type=sync --nthreads 16 --thread-pin 1 --nsteps=100000 --max-msg-size=8
