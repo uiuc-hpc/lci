2b85af99951550fff662045bca9d453f4f012678
Jiakun Yan <jiakunyan1998@gmail.com>
2025-03-02 23:07:49 -0600

    fix format.sh

--- format.sh
+++ format.sh
@@ -2,14 +2,15 @@
 
 echo "Formating c/c++ files..."
 shopt -s globstar # to activate the ** globbing
-if ! command -v clang-format &> /dev/null; then
-    alias clang-format='clang-format-11'
+clang_format_exe='clang-format'
+if ! command -v ${clang_format_exe} &> /dev/null; then
+    clang_format_exe='clang-format-11'
 fi
-clang-format --version
-clang-format -i $(find lct -type f -name "*.h" -o -name "*.c" -o -name "*.hpp" -o -name "*.cpp")
-clang-format -i $(find src -type f -name "*.h" -o -name "*.c" -o -name "*.hpp" -o -name "*.cpp")
-clang-format -i $(find examples -type f -name "*.h" -o -name "*.c" -o -name "*.hpp" -o -name "*.cpp")
-clang-format -i $(find tests -type f -name "*.h" -o -name "*.c" -o -name "*.hpp" -o -name "*.cpp")
+${clang_format_exe} --version
+${clang_format_exe} -i $(find lct -type f -name "*.h" -o -name "*.c" -o -name "*.hpp" -o -name "*.cpp")
+${clang_format_exe} -i $(find src -type f -name "*.h" -o -name "*.c" -o -name "*.hpp" -o -name "*.cpp")
+${clang_format_exe} -i $(find examples -type f -name "*.h" -o -name "*.c" -o -name "*.hpp" -o -name "*.cpp")
+${clang_format_exe} -i $(find tests -type f -name "*.h" -o -name "*.c" -o -name "*.hpp" -o -name "*.cpp")
 
 echo "Formating cmake files..."
 cmake-format --version
