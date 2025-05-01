f00ed81b17358631dd9f8e4aefd10199aa233681
Jiakun Yan <jiakunyan1998@gmail.com>
2025-04-30 20:30:06 -0500

    add bench_atomic

--- benchmarks/resources/bench_atomic.cpp
+++ benchmarks/resources/bench_atomic.cpp
@@ -1,6 +1,7 @@
 #include <getopt.h>
 #include <thread>
 #include <chrono>
+#include <atomic>
 
 #include "lct.h"
 #include "lci.hpp"
@@ -10,29 +11,17 @@
 struct config_t {
   int nthreads = 16;
   int niters = 1000;
-  int window = 1;
-  int nbytes = 64;
 } config;
 
 LCT_tbarrier_t g_tbarrier;
+std::atomic<uint64_t> g_counter = 0;
 
 void worker(int id) {
   util::pin_thread_to_cpu(id);
-  std::vector<void*> buffers(config.window);
   LCT_tbarrier_arrive_and_wait(g_tbarrier);
   auto start = std::chrono::high_resolution_clock::now();
   for (int i = 0; i < config.niters; i++) {
-    for (int j = 0; j < config.window; j++) {
-      void *ret = malloc(config.nbytes);
-      if (ret == nullptr) {
-        fprintf(stderr, "malloc failed\n");
-        exit(1);
-      }
-      buffers[j] = ret;
-    }
-    for (int j = config.window - 1; j >= 0; j--) {
-      free(buffers[j]);
-    }
+    g_counter.fetch_add(1, std::memory_order_relaxed);
   }
   LCT_tbarrier_arrive_and_wait(g_tbarrier);
   auto end = std::chrono::high_resolution_clock::now();
@@ -41,8 +30,8 @@ void worker(int id) {
   if (id == 0) {
     printf("Elapsed time: %.2f s\n", elapsed_s);
     printf("Per-operation time: %.2f us\n",
-           (elapsed_s * 1e6) / (config.niters * config.window));
-    double throughput_per_thread = (static_cast<double>(config.niters) * config.window) / (elapsed_s * 1e6);
+           (elapsed_s * 1e6) / config.niters);
+    double throughput_per_thread = static_cast<double>(config.niters) / (elapsed_s * 1e6);
     printf("Throughput per thread: %.2f Mops/s\n",
            throughput_per_thread);
     printf("Throughput: %.2f Mops/s\n", throughput_per_thread * config.nthreads);
@@ -55,10 +44,6 @@ int main(int argc, char** argv) {
     &config.nthreads);
   LCT_args_parser_add(argsParser, "niters", required_argument,
       &config.niters);
-  LCT_args_parser_add(argsParser, "window", required_argument,
-      &config.window);
-  LCT_args_parser_add(argsParser, "nbytes", required_argument,
-      &config.nbytes);
   LCT_args_parser_parse(argsParser, argc, argv);
   LCT_args_parser_print(argsParser, true);
   LCT_args_parser_free(argsParser);
