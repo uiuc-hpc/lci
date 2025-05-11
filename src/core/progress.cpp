28530aa8007a51ac5de968f107ea92b1b5ce6f98
Jiakun Yan <jiakunyan1998@gmail.com>
2025-05-10 23:30:52 -0500

    progress debug logging

--- src/core/progress.cpp
+++ src/core/progress.cpp
@@ -179,6 +179,11 @@ void progress_read(const net_status_t& net_status)
   }
 }
 
+// for logging purposes
+const uint64_t PROGRESS_LOG_INTERVAL = 1;  // 1s
+LCT_time_t last_update_time = 0;
+__thread uint64_t tls_progress_counter = 0;
+
 error_t progress_x::call_impl(runtime_t runtime, device_t device,
                               endpoint_t endpoint) const
 {
@@ -212,6 +217,19 @@ error_t progress_x::call_impl(runtime_t runtime, device_t device,
     }
     device.p_impl->refill_recvs();
   }
+
+  // Log progress every 1s
+#ifdef LCI_DEBUG
+  if (++tls_progress_counter % 1000000 == 0) {
+    // check the timer every 1M progress
+    LCT_time_t now = LCT_now();
+    if (LCT_time_to_s(now - last_update_time) > PROGRESS_LOG_INTERVAL) {
+      last_update_time = now;
+      LCI_DBG_Log(LOG_TRACE, "progress", "Progressed %lu times\n",
+                  tls_progress_counter);
+    }
+  }
+#endif
   return error;
 }
 
