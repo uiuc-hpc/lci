d62f91551bbd9bb185f76617100ea95b529fa352
Jiakun Yan <jiakunyan1998@gmail.com>
2026-06-26 15:04:13 -0700

    Apply CI formatting

--- tests/unit/cxx11/check_tcp_no_std_thread.cmake
+++ tests/unit/cxx11/check_tcp_no_std_thread.cmake
@@ -8,8 +8,8 @@ file(READ "${BACKEND_SOURCE}" backend_source)
 string(FIND "${backend_source}" "std::thread" std_thread_pos)
 if(NOT std_thread_pos EQUAL -1)
   message(
-    FATAL_ERROR
-      "TCP PMI backend must not create std::thread background workers")
+    FATAL_ERROR "TCP PMI backend must not create std::thread background workers"
+  )
 endif()
 string(FIND "${backend_source}" "#include <thread>" include_thread_pos)
 if(NOT include_thread_pos EQUAL -1)
