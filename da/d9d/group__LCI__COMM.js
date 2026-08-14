000433b694cd47161ec7df940daccb0f861899a1
JiakunYan <JiakunYan@users.noreply.github.com>
2026-08-14 17:25:00 +0000

    deploy: 4a647c91a2e42311e9b9f910294f534aebeb7b60

--- da/d9d/group__LCI__COMM.js
+++ da/d9d/group__LCI__COMM.js
@@ -1,6 +1,5 @@
 var group__LCI__COMM =
 [
-    [ "get_max_bcopy_size", "dc/d9c/group__LCI__GET__MAX__BCOPY__SIZE.html", "dc/d9c/group__LCI__GET__MAX__BCOPY__SIZE" ],
     [ "post_comm", "d5/d13/group__LCI__POST__COMM.html", "d5/d13/group__LCI__POST__COMM" ],
     [ "post_am", "dd/d27/group__LCI__POST__AM.html", "dd/d27/group__LCI__POST__AM" ],
     [ "post_send", "db/dc3/group__LCI__POST__SEND.html", "db/dc3/group__LCI__POST__SEND" ],
@@ -9,5 +8,6 @@ var group__LCI__COMM =
     [ "post_get", "de/d45/group__LCI__POST__GET.html", "de/d45/group__LCI__POST__GET" ],
     [ "progress", "dd/d9e/group__LCI__PROGRESS.html", "dd/d9e/group__LCI__PROGRESS" ],
     [ "test_drained", "d6/d70/group__LCI__TEST__DRAINED.html", "d6/d70/group__LCI__TEST__DRAINED" ],
-    [ "wait_drained", "d6/d7b/group__LCI__WAIT__DRAINED.html", "d6/d7b/group__LCI__WAIT__DRAINED" ]
+    [ "wait_drained", "d6/d7b/group__LCI__WAIT__DRAINED.html", "d6/d7b/group__LCI__WAIT__DRAINED" ],
+    [ "get_max_bcopy_size", "dc/d9c/group__LCI__GET__MAX__BCOPY__SIZE.html", "dc/d9c/group__LCI__GET__MAX__BCOPY__SIZE" ]
 ];
